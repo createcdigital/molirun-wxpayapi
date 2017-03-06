e2d90db14085047c6685c7ac3f5791c13e474871
coton_chen <coton.chen@gmail.com>
2017-03-06 16:54:48 +0800

    first commit

---
+++ wxpay/pub/pay.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+ini_set('date.timezone','Asia/Shanghai');
+//error_reporting(E_ERROR);
+require_once "../lib/WxPay.Api.php";
+require_once "WxPay.JsApiPay.php";
+require_once 'log.php';
+
+// param
+$openid      = $_POST['openid'];
+$grouptype   = $_POST['grouptype'];
+
+// body content
+$body        = $packagetype < 2 ? "100元一般跑" : "200元家庭跑";
+$fee         = $packagetype < 2 ? "10000" : "20000";
+$outtradeno  = WxPayConfig::MCHID.date("YmdHis");
+$tools       = new JsApiPay();
+
+// generate JsApi
+$input = new WxPayUnifiedOrder();
+$input->SetBody($body);
+$input->SetAttach($body);
+$input->SetOut_trade_no($outtradeno);
+$input->SetTotal_fee($fee);
+$input->SetTime_start(date("YmdHis"));
+$input->SetTime_expire(date("YmdHis", time() + 600));
+$input->SetGoods_tag($body);
+$input->SetNotify_url("https://pay.wechat.createcdigital.com/molirunh5170308/wxpay/pub/notify.php");
+$input->SetTrade_type("JSAPI");
+$input->SetOpenid($openid);
+$order = WxPayApi::unifiedOrder($input);
+$jsApiParameters = $tools->GetJsApiParameters($order);
+
+echo $jsApiParameters;
